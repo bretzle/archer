44d1fce022363b80b298d7074543f7e0a067d380
bretzle <johnfish218@gmail.com>
2020-07-21 16:31:02 -0500

    formatting

--- src/app_bar.rs
+++ src/app_bar.rs
@@ -30,132 +30,6 @@ mod system;
 
 pub type RedrawReason = String;
 
-pub fn redraw(reason: RedrawReason) -> WinApiResult<()> {
-	let ret = unsafe {
-		let app = INSTANCE.get_mut().unwrap();
-		app.redraw_reason = reason;
-		let hwnd = app.window.unwrap();
-		SendMessageA(hwnd as HWND, WM_PAINT, 0, 0) as i32
-	};
-
-	if ret == 0 {
-		Ok(())
-	} else {
-		Err(WinApiError::Err(ret))
-	}
-}
-
-pub fn create() {
-	info!("Creating appbar");
-	let name = "app_bar";
-	let app = unsafe { INSTANCE.get().unwrap() };
-	let config = app.config;
-
-	let height = config.height;
-	let display_width = app.display.width;
-
-	let window = &app.window;
-	let channel = &app.channel.sender;
-
-	app.components.values().for_each(|component| {
-		info!("Setting up component: {:?}", component);
-
-		thread::spawn(move || loop {
-			thread::sleep(component.interval());
-			if window.is_none() {
-				break;
-			}
-			channel
-				.send(Event::RedrawAppBar(component.reason()))
-				.expect("Failed to send redraw event");
-		});
-	});
-
-	thread::spawn(move || unsafe {
-		let instance = GetModuleHandleA(ptr::null_mut()).as_result().unwrap();
-		let background_brush = CreateSolidBrush(config.bg_color as u32)
-			.as_result()
-			.unwrap();
-
-		let class = WNDCLASSA {
-			hInstance: instance as HINSTANCE,
-			lpszClassName: name.as_ptr() as *const i8,
-			lpfnWndProc: Some(window_cb),
-			hbrBackground: background_brush as HBRUSH,
-			..WNDCLASSA::default()
-		};
-
-		RegisterClassA(&class).as_result().unwrap();
-
-		let window_handle = CreateWindowExA(
-			winapi::um::winuser::WS_EX_NOACTIVATE | winapi::um::winuser::WS_EX_TOPMOST,
-			name.as_ptr() as *const i8,
-			name.as_ptr() as *const i8,
-			winapi::um::winuser::WS_POPUPWINDOW & !winapi::um::winuser::WS_BORDER,
-			0,
-			0,
-			display_width,
-			height,
-			ptr::null_mut(),
-			ptr::null_mut(),
-			instance as HINSTANCE,
-			ptr::null_mut(),
-		)
-		.as_result()
-		.unwrap();
-
-		let app = INSTANCE.get_mut().unwrap();
-
-		app.window = Some(window_handle as i32);
-		app.init_draw_data();
-
-		let app = INSTANCE.get().unwrap();
-
-		let draw_data = app.draw_data.as_ref().unwrap();
-
-		let hwnd = app.show();
-
-		for component in app.components.values() {
-			component
-				.draw(draw_data, DeviceContext::new(hwnd).unwrap())
-				.expect("Failed to draw datetime")
-		}
-
-		let mut msg: MSG = MSG::default();
-		while GetMessageW(&mut msg, window_handle, 0, 0) > 0 {
-			TranslateMessage(&msg);
-			DispatchMessageW(&msg);
-		}
-	});
-
-	thread::spawn(|| unsafe {
-		let mut msg: MSG = MSG::default();
-
-		debug!("Registering win event hook");
-
-		let _hook = SetWinEventHook(
-			EVENT_MIN,
-			EVENT_MAX,
-			ptr::null_mut(),
-			Some(win_event_handler),
-			0,
-			0,
-			0,
-		)
-		.as_result()
-		.unwrap();
-
-		loop {
-			while PeekMessageW(&mut msg, 0 as HWND, 0, 0, PM_REMOVE) > 0 {
-				TranslateMessage(&msg);
-				DispatchMessageW(&msg);
-			}
-
-			thread::sleep(Duration::from_millis(5));
-		}
-	});
-}
-
 #[derive(Debug, Default)]
 pub struct AppBar {
 	display: Display,
@@ -196,7 +70,9 @@ impl AppBar {
 		thread::spawn(move || {
 			let receiver = self.channel.receiver.clone();
 
-			create();
+			self.init_components();
+			self.init_window();
+			self.init_winevent();
 
 			loop {
 				select! {
@@ -210,7 +86,7 @@ impl AppBar {
 
 	fn handle_event(&'static self, msg: Event) {
 		match msg {
-			Event::RedrawAppBar(reason) => redraw(reason).unwrap(),
+			Event::RedrawAppBar(reason) => self.redraw(reason).unwrap(),
 			Event::WinEvent(_) => {
 				if util::is_fullscreen() {
 					self.hide();
@@ -230,19 +106,145 @@ impl AppBar {
 		})
 	}
 
-	pub fn hide(&'static self) {
+	fn hide(&'static self) {
 		unsafe {
 			let hwnd = self.window.unwrap() as HWND; // Need to eager evaluate else there is a deadlock
 			ShowWindow(hwnd, SW_HIDE);
 		}
 	}
 
-	pub fn show(&'static self) -> HWND {
+	fn show(&'static self) -> HWND {
 		unsafe {
 			let hwnd = self.window.unwrap() as HWND; // Need to eager evaluate else there is a deadlock
 			ShowWindow(hwnd, SW_SHOW);
 			hwnd
 		}
+	}
+
+	fn redraw(&'static self, reason: RedrawReason) -> WinApiResult<()> {
+		let ret = unsafe {
+			let app = INSTANCE.get_mut().unwrap();
+			app.redraw_reason = reason;
+			let hwnd = app.window.unwrap();
+			SendMessageA(hwnd as HWND, WM_PAINT, 0, 0) as i32
+		};
+
+		if ret == 0 {
+			Ok(())
+		} else {
+			Err(WinApiError::Err(ret))
+		}
+	}
+
+	fn init_window(&'static self) {
+		info!("Creating appbar");
+		let name = "app_bar";
+		let config = self.config;
+
+		let height = config.height;
+		let display_width = self.display.width;
+
+		thread::spawn(move || unsafe {
+			let instance = GetModuleHandleA(ptr::null_mut()).as_result().unwrap();
+			let background_brush = CreateSolidBrush(config.bg_color as u32)
+				.as_result()
+				.unwrap();
+
+			let class = WNDCLASSA {
+				hInstance: instance as HINSTANCE,
+				lpszClassName: name.as_ptr() as *const i8,
+				lpfnWndProc: Some(window_cb),
+				hbrBackground: background_brush as HBRUSH,
+				..WNDCLASSA::default()
+			};
+
+			RegisterClassA(&class).as_result().unwrap();
+
+			let window_handle = CreateWindowExA(
+				winapi::um::winuser::WS_EX_NOACTIVATE | winapi::um::winuser::WS_EX_TOPMOST,
+				name.as_ptr() as *const i8,
+				name.as_ptr() as *const i8,
+				winapi::um::winuser::WS_POPUPWINDOW & !winapi::um::winuser::WS_BORDER,
+				0,
+				0,
+				display_width,
+				height,
+				ptr::null_mut(),
+				ptr::null_mut(),
+				instance as HINSTANCE,
+				ptr::null_mut(),
+			)
+			.as_result()
+			.unwrap();
+
+			{
+				let app = INSTANCE.get_mut().unwrap();
+				app.window = Some(window_handle as i32);
+				app.init_draw_data();
+			}
+
+			let draw_data = self.draw_data.as_ref().unwrap();
+			let hwnd = self.show();
+
+			self.components.values().for_each(|component| {
+				component
+					.draw(draw_data, DeviceContext::new(hwnd).unwrap())
+					.expect("Failed to draw component")
+			});
+
+			let mut msg: MSG = MSG::default();
+			while GetMessageW(&mut msg, window_handle, 0, 0) > 0 {
+				TranslateMessage(&msg);
+				DispatchMessageW(&msg);
+			}
+		});
+	}
+
+	fn init_components(&'static self) {
+		self.components.values().for_each(|component| {
+			info!("Setting up component: {:?}", component);
+
+			thread::spawn(move || loop {
+				thread::sleep(component.interval());
+				if *&self.window.is_none() {
+					break;
+				}
+				&self
+					.channel
+					.sender
+					.send(Event::RedrawAppBar(component.reason()))
+					.expect("Failed to send redraw event");
+			});
+		});
+	}
+
+	fn init_winevent(&'static self) {
+		thread::spawn(|| unsafe {
+			let mut msg: MSG = MSG::default();
+
+			debug!("Registering win event hook");
+
+			let _hook = SetWinEventHook(
+				EVENT_MIN,
+				EVENT_MAX,
+				ptr::null_mut(),
+				Some(win_event_handler),
+				0,
+				0,
+				0,
+			)
+			.as_result()
+			.unwrap();
+
+			loop {
+				while PeekMessageW(&mut msg, 0 as HWND, 0, 0, PM_REMOVE) > 0 {
+					TranslateMessage(&msg);
+					DispatchMessageW(&msg);
+				}
+
+				thread::sleep(Duration::from_millis(5));
+			}
+		});
 	}
 }
 
