d052158d8f59f6dd26eba0c16dd24aeedebda2ae
bretzle <johnfish218@gmail.com>
2020-07-22 17:12:46 -0500

    basic outline for functionality

--- src/wtm.rs
+++ src/wtm.rs
@@ -1,8 +1,12 @@
 use crate::{config::Config, INSTANCE};
+use crossbeam_channel::select;
+use std::thread;
+use winsapi::EventChannel;
 
 #[derive(Debug, Default)]
 pub struct WTM {
 	config: Config,
+	channel: EventChannel<Event>,
 }
 
 impl WTM {
@@ -19,6 +23,24 @@ impl WTM {
 	}
 
 	pub fn start(&'static mut self) {
-		loop {}
+		thread::spawn(move || {
+			let receiver = self.channel.receiver.clone();
+
+			self.register_keybinds();
+
+			loop {
+				select! {
+					recv(receiver) -> msg => {
+						self.handle_event(msg.unwrap());
+					}
+				}
+			}
+		});
 	}
+
+	fn register_keybinds(&'static self) {}
+
+	fn handle_event(&'static self, msg: Event) {}
 }
+
+pub enum Event {}
